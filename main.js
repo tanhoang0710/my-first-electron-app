948a843e1350d58fcbef2f9b888def7e781e61f7
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-14 13:29:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+const { app, BrowserWindow, Menu, shell } = require('electron');
+const path = require('path');
+
+const menuItems = [
+	{
+		label: 'Menu',
+		submenu: [
+			{
+				label: 'About',
+			},
+		],
+	},
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Learn More',
+				click: async () => {
+					await shell.openExternal('https://google.com');
+				},
+			},
+			{
+				label: 'New window',
+				click: async () => {
+					const win2 = new BrowserWindow({
+						height: 300,
+						width: 400,
+						show: false,
+						backgroundColor: '#2e2c29',
+						movable: false,
+					});
+
+					// win2.loadFile('index2.html');
+					win2.loadURL('https://www.google.com');
+					win2.once('ready-to-show', () => {
+						win2.show();
+					});
+				},
+			},
+			{
+				type: 'separator',
+			},
+			{
+				label: 'Exit',
+				click: () => {
+					app.quit();
+				},
+			},
+			// {
+			// 	role: 'close',
+			// },
+		],
+	},
+	{
+		label: 'Window',
+		submenu: [
+			{
+				role: 'close',
+			},
+			{
+				role: 'Minimize',
+			},
+		],
+	},
+];
+
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		height: 500,
+		width: 800,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+
+	win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
